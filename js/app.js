cc4f50aa1ac752f0efa7f843cf161f2f4ed46a60
Christian Stevens <stev1905@gmail.com>
2016-07-25 20:55:47 -0400

    Jquery submit button adds lists to list

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 $( document ).ready(function() {
 	$("#submit").click(function(){
-		//alert("Submit button works!!");
-		//$("#text").css("background-color", "red");
+		//alert("Submit button works!!")
+		$('.items').append('<span class="glyphicon glyphicon-trash"></span>');
   		$('.items').append($('<li>', {
-         	text: $('#text').val(),
-         	$("#text").val(''),
-    	});
-	});   
+         	text: $('#text').val() ,
+    	}));
+    	$("#text").val('');
+	});
+	$('#text').keypress(function(e){
+    	if(e.which == 13){ 	
+    		$('.items').append('<span class="glyphicon glyphicon-trash"></span>');
+    		$('.items').append($('<li>', {
+         		text: $('#text').val(),
+    		}));
+    		$("#text").val('');
+    	}
+	}); 
 });
