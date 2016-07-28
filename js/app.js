56bf8c45ee09ba06b5b0fc3144524444c719a98f
Christian Stevens <stev1905@gmail.com>
2016-07-27 20:14:50 -0400

    events added to remove and strikethrough shopping list

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
 $( document ).ready(function() {
-	$("#submit").click(function(){
-
+		$("#submit").click(function(){
 		//alert("Submit button works!!")
-  		$('.items').append('<li>' + '<input type="checkbox" name="checkbox" id="checkbox"> ' + $('#text').val() + ' <span class="glyphicon glyphicon-trash"></span></li>')
-    	$("#text").val('');
-    });
-	$('#text').keypress(function(e){
-	    	if(e.which == 13){ 	
-	    	$('.items').append('<li>' + '<input type="checkbox" name="checkbox" id="checkbox"> ' + $('#text').val() + ' <span class="glyphicon glyphicon-trash"></span></li>')
-    		$("#text").val('');
-		}
-
-    });
-    $(document).on('change', '[type=checkbox]', function () {
-		alert('clicked');
+	    $('.items').append('<li>' + '<input type="checkbox" name="checkbox" id="checkbox"> ' + $('#text').val() + ' <span class="glyphicon glyphicon-trash" id="icon"></span></li>')
+	    $("#text").val('');
 	});
+		$('#text').keypress(function(e){
+      	if(e.which == 13){  
+      	$('.items').append('<li>' + '<input type="checkbox" name="checkbox" id="checkbox"> ' + $('#text').val() + ' <span class="glyphicon glyphicon-trash" id="icon"></span></li>')
+      	$("#text").val('');
+  	}
+});
+    	$(document).on('change', '[type=checkbox]', function () {
+  		if ($('input#checkbox').is(':checked')) {
+   	    //alert('THis box is checked')
+    	$(this).parent().css("text-decoration", "line-through")
+  	}
+  		else{
+   		//alert('this box is unchecked')
+   		$(this).parent().css("text-decoration", "none")
+  	}
+});
+$(document).on('click', '#icon', function () {
+		//alert('clicked on trash icon') 
+		$(this).parent().remove()
+ 	});
 });
